818181849b26433ebc696685f82d8bf94b4e29b1
Md. Sabik Alam Rahat <sabikrahat72428@gmail.com>
2025-01-07 16:11:25 +0600

    web.yml file added

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Rive Playground',
-      showPerformanceOverlay: true,
+      showPerformanceOverlay: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
